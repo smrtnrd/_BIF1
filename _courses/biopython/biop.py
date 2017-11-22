792261d953097d868261e35cac0407a9aaa83ad9
Berthin Biyong <bbitj026@uottawa.ca>
2017-11-22 12:02:43 -0500

    # update

---
+++ _courses/biopython/biop.py
@@ -0,0 +1,21 @@
+from Bio.Seq import Seq
+
+my_seq = Seq("ACTACGACTCAG")
+
+print(my_seq)
+
+print(my_seq.complement())
+
+
+
+print(my_seq[0])
+print(my_seq[4])
+print(my_seq[-1])
+
+print(len(my_seq))
+
+print(my_seq[4:8])
+
+#print(my_seq.alphabet)
+
+
